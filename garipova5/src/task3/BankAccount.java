e30c0a99068a89057307ea1e9a9f19adfd9383a3
Aliya Garipova <garipova.liusha@gmail.com>
2026-09-09 11:11:57 +0300

    Add files via upload

---
+++ garipova5/src/task3/BankAccount.java
@@ -0,0 +1,17 @@
+package task3;
+public class BankAccount {
+    private long moneyAmount;
+
+    public long getMoneyAmount() {
+        return moneyAmount;
+    }
+
+    public void setMoneyAmount(long newMoneyAmount) {
+        moneyAmount = newMoneyAmount;
+    }
+
+    public void withdrawAll() {
+        System.out.println("Со счёта снято " + moneyAmount + " р.");
+        moneyAmount = 0;
+    }
+}
